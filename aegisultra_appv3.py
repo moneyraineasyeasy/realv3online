c16903835aea715a0bf36d2b3f9ee4f393666f43
moneyraineasyeasy <hiyagoodmorning@gmail.com>
2026-09-22 01:27:41 +0800

    Fix formatting in market implied probability change

--- aegisultra_appv3.py
+++ aegisultra_appv3.py
@@ -1297,7 +1297,7 @@ def candidate_dataframe(
                 )
             ),
             "市場變動": (
-                f"{safe_float(
+                "{safe_float(
                     shift.get(
                         'market_implied_probability_change_pp'
                     ),
